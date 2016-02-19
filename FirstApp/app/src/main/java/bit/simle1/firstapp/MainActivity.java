0445bf581a0cea89dfef4c934402720775e0055f
leonardsim <SIMLE1@student.op.ac.nz>
2016-02-19 15:01:14 +1300

    Practical 1.2a - First Code
    Randomly generate a dog breed to display on the dogBreedText

--- FirstApp/app/src/main/java/bit/simle1/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/bit/simle1/firstapp/MainActivity.java
@@ -2,6 +2,9 @@ package bit.simle1.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dogTextRandom = (TextView)findViewById(R.id.dogBreedText);
+
+        Random randNum = new Random();
+        int dogValue = randNum.nextInt(4);
+
+        switch (dogValue)
+        {
+            case 0:
+                dogTextRandom.setText("Poodle");
+                break;
+            case 1:
+                dogTextRandom.setText("Labrador");
+                break;
+            case 2:
+                dogTextRandom.setText("Shar Pei");
+                break;
+            case 3:
+                dogTextRandom.setText("Newfoundland");
+                break;
+        }
     }
 }
