9bc1f00bff7df2cbf2bca7ed9f9a1ae800cd8a19
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-04 01:29:59 +1300

    Added thread to splash class

--- MusicSpinner/app/src/main/java/bit/simle1/musicspinner/Splash.java
+++ MusicSpinner/app/src/main/java/bit/simle1/musicspinner/Splash.java
@@ -1,5 +1,6 @@
 package bit.simle1.musicspinner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,28 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    //Thread will sleep for 5 seconds
+                    sleep(5 * 1000);
+
+                    // After 5 seconds, it will redirect to the main activity
+                    Intent splashIntent = new Intent(Splash.this, MainActivity.class);
+
+                    // Start the activity
+                    startActivity(splashIntent);
+
+                    // End activity
+                    finish();
+                }
+                catch (InterruptedException e) {
+                }
+
+            }
+        };
+
+        // Start thread
+        background.start();
     }
 }
