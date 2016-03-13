1b611fba61cc106ae8bf602e3915ce8b44f7f097
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-14 12:46:37 +1300

    Created the handler for button image

--- Fragment1/app/src/main/java/bit/simle1/fragment1/MainActivity.java
+++ Fragment1/app/src/main/java/bit/simle1/fragment1/MainActivity.java
@@ -1,7 +1,12 @@
 package bit.simle1.fragment1;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain references from both buttons
+        Button btnImage = (Button) findViewById(R.id.btnImage);
+        Button btnList = (Button) findViewById(R.id.btnList);
+
+        // Button creates the handler on click
+        btnImage.setOnClickListener(new onClickBtnImgHandler());
+    }
+
+    public class onClickBtnImgHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            // Create instance of image fragment class
+            Fragment dynamicFragment =  new ShowImage();
+            // Fragment Manager used to replace the placeholder with the Fragment
+            FragmentManager fm = getFragmentManager();
+
+            // Begin transaction by obtaining FragmentTransaction object
+            FragmentTransaction ft = fm.beginTransaction();
+
+            //Replaces the fragment_container with the dynamicFragment object
+            ft.replace(R.id.fragment_container, dynamicFragment);
+
+            //Commits the changes
+            ft.commit();
+        }
     }
 }
---
+++ Fragment1/app/src/main/java/bit/simle1/fragment1/ShowImage.java
@@ -0,0 +1,28 @@
+package bit.simle1.fragment1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bit.simle1.fragment1.R;
+
+/**
+ * Created by simle1 on 11/03/2016.
+ */
+public class ShowImage extends Fragment {
+
+    // LayoutInflater inflater: a system object that knows how to translate XML into controls
+    // ViewGroup container: Activity who is instantiating the fragment
+    // Bundle savedInstanceState: state information if this Fragment is coming back from Pause or Stop
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // LayoutInflater translate the layout into a collection of controls (View) and returns the VIew
+        View fragmentView = inflater.inflate(R.layout.show_image, container, false);
+
+        return fragmentView;
+    }
+}
