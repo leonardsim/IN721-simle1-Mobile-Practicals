4d2db0d5128d060740db052e6aae5a8f6618d441
leonardsim <SIMLE1@student.op.ac.nz>
2016-03-16 15:27:38 +1300

    Returns data

--- TextColourPassing/app/src/main/java/bit/simle1/textcolourpassing/SettingsActivity.java
+++ TextColourPassing/app/src/main/java/bit/simle1/textcolourpassing/SettingsActivity.java
@@ -1,7 +1,12 @@
 package bit.simle1.textcolourpassing;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        returnColour();
+    }
+
+    public void returnColour()
+    {
+        TextView tvColour = (TextView) findViewById(R.id.tvColourText);
+
+        int textColour = tvColour.getCurrentTextColor();
+
+        // Create an intent
+        Intent returnIntent = new Intent();
+
+        // Load up the return data
+        returnIntent.putExtra("txtColour", textColour);
+
+        // Set the return code
+        setResult(Activity.RESULT_OK, returnIntent);
+
+        // Pop off the Activity Stack
+        finish();
     }
 }
