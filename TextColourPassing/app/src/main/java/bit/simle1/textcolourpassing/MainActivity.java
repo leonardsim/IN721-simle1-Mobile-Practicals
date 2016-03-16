ea0c6c672e85897c477f41802f50221df0ced133
leonardsim <SIMLE1@student.op.ac.nz>
2016-03-16 14:51:46 +1300

    Added onclick handler

--- TextColourPassing/app/src/main/java/bit/simle1/textcolourpassing/MainActivity.java
+++ TextColourPassing/app/src/main/java/bit/simle1/textcolourpassing/MainActivity.java
@@ -2,6 +2,8 @@ package bit.simle1.textcolourpassing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get reference to button
+        Button btnColour = (Button) findViewById(R.id.btnColour);
+    }
+
+    public class RetrieveDataOnClickHandler implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            
+        }
     }
 }
