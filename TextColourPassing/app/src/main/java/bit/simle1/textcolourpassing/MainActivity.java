b15ca2ad57689519bf26128dca98977528546d30
leonardsim <SIMLE1@student.op.ac.nz>
2016-03-16 15:29:43 +1300

    override onActivityResult()
    Will fetch the data from settings & if it checks out, it will set the
    text colour of the main text view to be the same as the one in Settings
    Activity

--- TextColourPassing/app/src/main/java/bit/simle1/textcolourpassing/MainActivity.java
+++ TextColourPassing/app/src/main/java/bit/simle1/textcolourpassing/MainActivity.java
@@ -1,9 +1,14 @@
 package bit.simle1.textcolourpassing;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +19,32 @@ public class MainActivity extends AppCompatActivity {
 
         // Get reference to button
         Button btnColour = (Button) findViewById(R.id.btnColour);
+        btnColour.setOnClickListener(new RetrieveDataOnClickHandler());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        // Use the requestCode
+        if ((requestCode == 0) && (resultCode == Activity.RESULT_OK))
+        {
+            // Fetch result
+            int colour = data.getIntExtra("txtColour", 0);
+
+            //Set textView's colour
+            TextView mainText = (TextView)findViewById(R.id.tvMain);
+
+            mainText.setTextColor(colour);
+        }
     }
 
     public class RetrieveDataOnClickHandler implements View.OnClickListener
     {
-
         @Override
         public void onClick(View v) {
-            
+            Intent changeActivity = new Intent(MainActivity.this, SettingsActivity.class);
+
+            startActivityForResult(changeActivity, 0);
         }
     }
 }
