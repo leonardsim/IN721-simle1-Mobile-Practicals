631c0a626786e6c5f052f834bf7ec035255c3205
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-22 21:46:54 +1300

    Created YesButtonHandler

---
+++ MusicSpinner/app/src/main/java/bit/simle1/musicspinner/AlertBuilderFragment.java
@@ -0,0 +1,26 @@
+package bit.simle1.musicspinner;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Asus on 22/3/2016.
+ */
+public class AlertBuilderFragment extends DialogFragment {
+
+    // Empty constructor
+    public AlertBuilderFragment(){}
+
+    public class YesButtonHandler implements DialogInterface.OnClickListener
+    {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+            MainActivity myActivity = (MainActivity) getActivity();
+            myActivity.giveMeMyData(true);
+        }
+    }
+}
