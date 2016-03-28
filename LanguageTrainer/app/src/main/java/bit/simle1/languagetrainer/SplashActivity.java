14b38a19d9028a1fcd5ad7cb4f5c5a623acfa066
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-29 11:59:23 +1300

    Created btnClickHandler to transition to next intent

--- LanguageTrainer/app/src/main/java/bit/simle1/languagetrainer/SplashActivity.java
+++ LanguageTrainer/app/src/main/java/bit/simle1/languagetrainer/SplashActivity.java
@@ -1,7 +1,10 @@
 package bit.simle1.languagetrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Create button reference and the listener
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new btnClickHandler());
+    }
+
+
+    public class btnClickHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            Intent quizIntent = new Intent(SplashActivity.this, QuizActivity.class);
+            startActivity(quizIntent);
+        }
     }
 }
