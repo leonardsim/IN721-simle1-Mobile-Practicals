da116721521cdaf4aaa33a4d3a5d65cb8d4835fb
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-29 14:44:42 +1300

    Created setQuestion method to add questions to questionList

--- LanguageTrainer/app/src/main/java/bit/simle1/languagetrainer/QuizActivity.java
+++ LanguageTrainer/app/src/main/java/bit/simle1/languagetrainer/QuizActivity.java
@@ -3,6 +3,7 @@ package bit.simle1.languagetrainer;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -24,6 +25,9 @@ public class  QuizActivity extends AppCompatActivity {
     FragmentManager fm;
     FragmentTransaction ft;
 
+    //Declare Resource
+    Resources resources;
+
     //Attributes
     int totalScore;
     int currentQuiz;
@@ -37,6 +41,7 @@ public class  QuizActivity extends AppCompatActivity {
         questionList = new ArrayList<>();
         totalScore = 0;
         currentQuiz = 0;
+        resources = getResources();
 
         //Set the fragment to replace the container with the quiz image layout
         dynamicFragment = new QuizImageFragment();
@@ -47,9 +52,25 @@ public class  QuizActivity extends AppCompatActivity {
 
     }
 
+    // Adds questions to the questionList
     public void setQuestion()
     {
+        // Masculine Article
+        questionList.add(new Question("Der", "Apfel (Apple)", resources.getDrawable(R.drawable.der_apfel)));
+        questionList.add(new Question("Der", "Baum (Tree)", resources.getDrawable(R.drawable.der_baum)));
+        questionList.add(new Question("Der", "Stuhl (Chair)", resources.getDrawable(R.drawable.der_stuhl)));
 
+        // Feminine Article
+        questionList.add(new Question("Die", "Ente (Duck)", resources.getDrawable(R.drawable.die_ente)));
+        questionList.add(new Question("Die", "Hexe (Witch)", resources.getDrawable(R.drawable.die_hexe)));
+        questionList.add(new Question("Die", "Kuh (Cow)", resources.getDrawable(R.drawable.die_kuh)));
+        questionList.add(new Question("Die", "Milch (Milk)", resources.getDrawable(R.drawable.die_milch)));
+        questionList.add(new Question("Die", "Strasse (Street)", resources.getDrawable(R.drawable.die_strasse)));
+
+        // Neutral Arcticle
+        questionList.add(new Question("Das", "Auto (Car)", resources.getDrawable(R.drawable.das_auto)));
+        questionList.add(new Question("Das", "Haus (House)", resources.getDrawable(R.drawable.das_haus)));
+        questionList.add(new Question("Das", "Schaf (Sheep)", resources.getDrawable(R.drawable.das_schaf)));
     }
 
 }
