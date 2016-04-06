673e844927dd2b8def42cfe4e062c96ca5293709
leonardsim <SIMLE1@student.op.ac.nz>
2016-04-06 17:13:33 +1200

    Completed CityCountryDatabase :100:  :100:

--- CityCountryDatabase/app/src/main/java/bit/simle1/citycountrydatabase/MainActivity.java
+++ CityCountryDatabase/app/src/main/java/bit/simle1/citycountrydatabase/MainActivity.java
@@ -4,8 +4,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +31,36 @@ public class MainActivity extends AppCompatActivity {
 
         // Populates the spinner with country name
         populateSpinner();
+
+        //Create button reference and set listener
+        Button btnShow = (Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new SetCitiesClickHandler());
     }
 
+    //Event Handler
+    public class SetCitiesClickHandler implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            // Create spinner reference
+            Spinner spnCountries = (Spinner) findViewById(R.id.spnCountries);
+
+            // Save the country name depending on what was selected from the spinner
+            String selectedCountry = spnCountries.getSelectedItem().toString();
+
+            // Gets cities name
+            String[] cities = getCities(selectedCountry);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, cities);
+
+            // Set the city name to be on the  list view
+            ListView lvCities = (ListView) findViewById(R.id.lvCities);
+            lvCities.setAdapter(adapter);
+        }
+    }
+
+    //Methods
     private void createCityTable()
     {
         // Drops the city table
@@ -92,5 +125,36 @@ public class MainActivity extends AppCompatActivity {
 
         // Return the country name array
         return displayCountryArray;
+    }
+
+    private String[] getCities(String selectedCountry)
+    {
+        // Obtain all unique country name
+        String selectQuery = "SELECT DISTINCT cityName FROM tblCity WHERE countryName = " + "\"" + selectedCountry + "\"";
+        Cursor recordSet = citiesDB.rawQuery(selectQuery, null);
+
+        // Gets the number of record
+        int recordCount = recordSet.getCount();
+
+        // Declare array and set the array size
+        String[] displayCityArray = new String[recordCount];
+
+        // Get the column index of cityName
+        int cityNameIndex = recordSet.getColumnIndex("cityName");
+
+        // Move to the first of the record
+        recordSet.moveToFirst();
+
+        for (int r = 0; r < recordCount; r++)
+        {
+            // Saved city name
+            displayCityArray[r] = recordSet.getString(cityNameIndex);
+
+            // Move to the next record
+            recordSet.moveToNext();
+        }
+
+        // Return the country name array
+        return displayCityArray;
     }
 }
