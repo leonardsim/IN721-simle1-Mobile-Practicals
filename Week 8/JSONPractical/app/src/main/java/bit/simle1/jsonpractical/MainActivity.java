019fe9d887c77956990d5fbd602fbcacf4b7d98b
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-11 14:18:19 +1200

    Created displayEventDescription method to display description with Toast

--- Week 8/JSONPractical/app/src/main/java/bit/simle1/jsonpractical/MainActivity.java
+++ Week 8/JSONPractical/app/src/main/java/bit/simle1/jsonpractical/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +17,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialise JSONInput
+        JSONInput = readEntireJSON("dunedin_events.json");
+
         // Create reference for button and set the listener
         Button btnFill = (Button) findViewById(R.id.btnFill);
         btnFill.setOnClickListener(new SetListEventHandler());
+
+        // Create reference for list view and set the listener
+        ListView lvEvents = (ListView) findViewById(R.id.lvEvents);
+        lvEvents.setOnItemClickListener(new SetListViewClickHandler());
     }
 
     //Event Handlers
@@ -35,9 +45,6 @@ public class MainActivity extends AppCompatActivity {
     {
         @Override
         public void onClick(View v) {
-
-            // Convert String to JSON Object
-            JSONInput = readEntireJSON("dunedin_events.json");
 
             // Declare ArrayList and initialise
             ArrayList<String> eventDisplayArray = populateArrayList(JSONInput);
@@ -48,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
             // Create ListView reference and bind the adapter to the list
             ListView lvEvents = (ListView) findViewById(R.id.lvEvents);
             lvEvents.setAdapter(adapter);
+        }
+    }
+
+    public class SetListViewClickHandler implements AdapterView.OnItemClickListener
+    {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            // Call display method
+            displayEventDescription(JSONInput, position);
         }
     }
 
@@ -119,5 +136,44 @@ public class MainActivity extends AppCompatActivity {
         return eventDisplayArray;
     }
 
+    public void displayEventDescription(String input, int position)
+    {
 
+        // Create reference to list view
+        ListView lvEvents = (ListView) findViewById(R.id.lvEvents);
+
+        try {
+
+            // Convert file string to JSON Object
+            JSONObject eventData = new JSONObject(input);
+
+            // Grab the value part of the data: key-value pair (Which is an object called events)
+            JSONObject eventObject = eventData.getJSONObject("events");
+
+            // Grab the value part of the data: key-value pair (Which is an array called event)
+            JSONArray eventArray = eventObject.getJSONArray("event");
+
+            // Loop through eventArray
+            for (int i = 0; i < eventArray.length(); i++)
+            {
+                // Get an element from the array
+                JSONObject currentEventObject = eventArray.getJSONObject(i);
+
+                // Access the title value
+                String currentEventTitle = currentEventObject.getString("title");
+
+                // Checks to see if the title and the item selected in the list view are equal
+                if(currentEventTitle.equals(lvEvents.getItemAtPosition(position).toString()))
+                {
+                    // Access the description value
+                    String currentEventDescription = currentEventObject.getString("description");
+
+                    // Call the toast to display the description value
+                    Toast.makeText(MainActivity.this, currentEventDescription, Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
