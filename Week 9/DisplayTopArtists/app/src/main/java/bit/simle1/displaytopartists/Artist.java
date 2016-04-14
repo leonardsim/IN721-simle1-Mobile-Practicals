ce3509d28a65e687ac8684847b1eef9de4bd51e4
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-14 18:57:13 +1200

    Created Artist java class

---
+++ Week 9/DisplayTopArtists/app/src/main/java/bit/simle1/displaytopartists/Artist.java
@@ -0,0 +1,17 @@
+package bit.simle1.displaytopartists;
+
+/**
+ * Created by Asus on 14/4/2016.
+ */
+public class Artist {
+    //Attributes
+    String name;
+    String listener;
+
+    //Constructor
+    public Artist(String name, String listener)
+    {
+        this.name = name;
+        this.listener = listener;
+    }
+}
