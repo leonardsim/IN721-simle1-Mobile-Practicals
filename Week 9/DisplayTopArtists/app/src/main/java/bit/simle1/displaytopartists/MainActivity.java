ee2f0888a4e4f299a2e04aa0080bf72c32660b1a
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-14 13:55:11 +1200

    Created SetListArtistHandler handler

--- Week 9/DisplayTopArtists/app/src/main/java/bit/simle1/displaytopartists/MainActivity.java
+++ Week 9/DisplayTopArtists/app/src/main/java/bit/simle1/displaytopartists/MainActivity.java
@@ -2,6 +2,8 @@ package bit.simle1.displaytopartists;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create reference for button and set listener
+        Button btnFill = (Button) findViewById(R.id.btnFill);
+        btnFill.setOnClickListener(new SetListArtistHandler());
     }
 
+    // Event Handler
+    public class SetListArtistHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
 
+        }
+    }
 }
