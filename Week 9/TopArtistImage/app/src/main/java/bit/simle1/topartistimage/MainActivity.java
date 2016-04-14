7da281d76b9fd8b08f0a15b2b6f56ee4d0b5ac7b
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-14 21:43:18 +1200

    Created SetArtistImageHandler handler

--- Week 9/TopArtistImage/app/src/main/java/bit/simle1/topartistimage/MainActivity.java
+++ Week 9/TopArtistImage/app/src/main/java/bit/simle1/topartistimage/MainActivity.java
@@ -2,6 +2,8 @@ package bit.simle1.topartistimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create button reference and set listener
+        Button btnShow = (Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new SetArtistImageHandler());
+    }
+
+    public class SetArtistImageHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
