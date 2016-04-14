cdeb399962ad21f81ee144b8c9389c43f1d2e8f0
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-14 14:33:51 +1200

    Updated doInBackground method

--- Week 9/DisplayTopArtists/app/src/main/java/bit/simle1/displaytopartists/MainActivity.java
+++ Week 9/DisplayTopArtists/app/src/main/java/bit/simle1/displaytopartists/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -33,7 +40,54 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... params)
         {
+            // String that will return the JSON data
+            String JSONString = null;
 
+            try {
+
+                // URL that will display all the top artists in JSON format
+                String urlString = "http://ws.audioscrobbler.com/2.0/?"
+                        + "method=chart.getTopArtists&limit=20&"
+                        + "api_key=5cff6bd4a02a240bbfef15567f21c45d&"
+                        + "format=json";
+
+                // Convert URL string to URLObject
+                URL URLObject = new URL(urlString);
+
+                // Create HttpURLConnection object via openConnection method of URLObject
+                HttpURLConnection connection = (HttpURLConnection) URLObject.openConnection();
+
+                // Send the IRL
+                connection.connect();
+
+                // If it doesn't return 200, no data has been received
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == 200)
+                {
+                    // Get an InputStream from the HttpURLConnection object and set up a Buffered Reader
+                    InputStream inputStream = connection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    // Read the input
+                    String responseString;
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    while ((responseString = bufferedReader.readLine()) != null)
+                    {
+                        stringBuilder = stringBuilder.append(responseString);
+                    }
+
+                    // Get the string from the stringBuilder
+                    JSONString = stringBuilder.toString();
+                }
+
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+
+            return JSONString;
         }
 
         @Override
