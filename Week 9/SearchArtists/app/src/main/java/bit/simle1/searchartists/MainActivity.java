dc6b86b9101a1ab9415c73779bdb1174d16638fd
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-14 20:18:58 +1200

    Created SetListArtistHandler

--- Week 9/SearchArtists/app/src/main/java/bit/simle1/searchartists/MainActivity.java
+++ Week 9/SearchArtists/app/src/main/java/bit/simle1/searchartists/MainActivity.java
@@ -2,6 +2,8 @@ package bit.simle1.searchartists;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create reference for button and set listener
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener();
     }
+
+    //Event Handler
+    public class SetListArtistHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    // Create inner class AsyncTask
+    
 }
