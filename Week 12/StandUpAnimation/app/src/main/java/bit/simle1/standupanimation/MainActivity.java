e55f0ac6c3967fd78a5ffcd331a837861a740607
leonardsim <simle1@STUDENT.op.ac.nz>
2016-05-22 23:43:06 +1200

    Got StandUp technique to work

--- Week 12/StandUpAnimation/app/src/main/java/bit/simle1/standupanimation/MainActivity.java
+++ Week 12/StandUpAnimation/app/src/main/java/bit/simle1/standupanimation/MainActivity.java
@@ -2,12 +2,34 @@ package bit.simle1.standupanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+
+        Button btnAnimate = (Button) findViewById(R.id.btnAnimate);
+        btnAnimate.setOnClickListener(new setStandUpImageHandler());
+    }
+
+    public class setStandUpImageHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            YoYo.with(Techniques.StandUp).duration(5000).playOn(iv);
+        }
     }
 }
