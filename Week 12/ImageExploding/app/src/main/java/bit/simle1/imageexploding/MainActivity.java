14adf95a277243b3fe2f82159d16b04982e5371c
leonardsim <simle1@STUDENT.op.ac.nz>
2016-05-22 23:25:16 +1200

    Got exploding to work

--- Week 12/ImageExploding/app/src/main/java/bit/simle1/imageexploding/MainActivity.java
+++ Week 12/ImageExploding/app/src/main/java/bit/simle1/imageexploding/MainActivity.java
@@ -1,13 +1,40 @@
 package bit.simle1.imageexploding;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.easyandroidanimations.library.Animation;
+import com.easyandroidanimations.library.AnimationListener;
+import com.easyandroidanimations.library.ExplodeAnimation;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = (ImageView)findViewById(R.id.imageView);
+        Button btnExplode = (Button)findViewById(0R.id.btnExplode);
+        btnExplode.setOnClickListener(new setImageToExplodeHandler());
+    }
+
+    public class setImageToExplodeHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            ExplodeAnimation ea = new ExplodeAnimation(iv);
+            ea.setExplodeMatrix(ExplodeAnimation.MATRIX_2X2);
+            ea.setInterpolator(new DecelerateInterpolator());
+            ea.setDuration(5000);
+            ea.animate();
+        }
     }
 }
